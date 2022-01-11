c1034b9f513e7e5cb09362a977d1446b38e8f201
Mickael Asseline <papamica@PAPAMICA-PCPRO.local>
2022-01-11 10:50:43 +0100

    Add comments

--- change-container-policy.sh
+++ change-container-policy.sh
@@ -2,6 +2,7 @@
 
 ERRORS=0
 
+# Gestion des arguments et message d'aide.
 if [ "$1" = "--help" ]; then
     echo "  This script can change policy of a container."
     echo "  Usage :"
@@ -21,13 +22,14 @@ else
     POLICY="$2"
 fi
 
-
+# Vérification que le container existe bien
 swift stat $CONTAINER
 if test $? -ne 0; then
     echo "[$(date +%Y-%m-%d_%H:%M:%S)]   ChangeContainerPolicy   ❌   ERROR : Container $CONTAINER does not exist !"
     exit
 fi
 
+# Récupération des ACL de lecture et d'éciture
 READACL=$(swift stat $CONTAINER | grep 'Read ACL' | awk '{print $3}')
 if [ -n $READACL ]; then
     ARGS=$(echo $ARGS --header "X-Container-Read: $READACL")
@@ -38,9 +40,11 @@ if [ -n $WRITEACL ]; then
     ARGS=$(echo $ARGS --header "X-Container-Read: $WRITEACL")
 fi
 
+# Création d'un container temporaire avec un nom random
 CONTAINERTEMP="$CONTAINER-temp-$RANDOM"
 swift post $CONTAINERTEMP
 
+# Copie des objects du container vers un container temporaire
 NBTOTALOBJECTS=$(swift stat $CONTAINER | grep Objects | awk '{print $2}') > /dev/null 2>&1
 NBOBJECT=0
 OBJECTSLIST=$(swift list $CONTAINER)
@@ -54,6 +58,7 @@ for OBJECT in $OBJECTSLIST; do
         fi
 done
 
+# Si pas d'erreurs dans la copie, suppression du container original
 if [ "$ERRORS" -eq "0" ]; then
     swift delete $CONTAINER > /dev/null 2>&1
     if test $? -eq 0; then
@@ -66,6 +71,7 @@ else
     exit
 fi
 
+# Création du container final avec la bonne policy
 swift post $CONTAINER -H "X-Storage-Policy: $POLICY" $ARGS > /dev/null 2>&1
 if test $? -eq 0; then
     echo "[$(date +%Y-%m-%d_%H:%M:%S)]   ChangeContainerPolicy   ✅   New container $CONTAINER with policy $POLICY successfully created."
@@ -73,6 +79,7 @@ else
     echo "[$(date +%Y-%m-%d_%H:%M:%S)]   ChangeContainerPolicy   ❌   ERROR : A problem was encountered during the create of container $CONTAINER with policy $POLICY."
 fi
 
+# Copie des objects du container temporaire vers le container final
 OBJECTSLISTTEMP=$(swift list $CONTAINERTEMP)
 NBOBJECT=0
 for OBJECT in $OBJECTSLISTTEMP; do
@@ -84,6 +91,7 @@ for OBJECT in $OBJECTSLISTTEMP; do
         fi
 done
 
+# Si pas d'erreurs dans la copie, suppression du container final
 if [ "$ERRORS" -eq "0" ]; then
     swift delete $CONTAINERTEMP > /dev/null 2>&1
     if test $? -eq 0; then
@@ -97,4 +105,5 @@ else
     exit
 fi
 
+# Affichage des informations du container final
 swift stat $CONTAINER
